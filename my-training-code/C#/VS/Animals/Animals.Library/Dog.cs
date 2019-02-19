6a1700c343ad96c555d3e8eeb2b2fd4f3c05d03c
Conrad <conrad.trohajr@gmail.com>
2019-02-19 15:00:57 -0500

    Created new project MoreAnimals

--- my-training-code/C#/VS/Animals/Animals.Library/Dog.cs
+++ my-training-code/C#/VS/Animals/Animals.Library/Dog.cs
@@ -4,8 +4,61 @@ namespace Animals.Library
 {
     public class Dog
     {
+        // Fields
+        // Declaring fields and getters and setters is NOT
+        // common practice in C#! Use properties instead
+        internal string Noise = "Woof!";
 
-        public string Noise = "Woof!";
+        //Getter
+        public string getNoise()
+        {
+            return Noise;
+        }
+
+        //Setter
+        public void setNoise(string newValue)
+        {
+            if(newValue == null || newValue.Length == 0)
+            {
+                // throwing an exception
+                throw new ArgumentException("Value must not be null or empty");
+            }
+            Noise = newValue;
+        }
+
+        // Instead of using getters and setters,
+        // in C# we have properties where other languages would just
+        // use fields on their own
+
+        // simplest property "auto-implemented"
+        // field generated behind the scenes to back the property
+        // "backing field"
+        // Usually properties have some backing field
+        public int id {get; set;} = 0;
+
+        // This is the manual version of auto-property
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            } 
+            set
+            {
+                // Implicit argument "value"
+                // could do null/empty checks, etc.
+                _name = value;
+            }
+        }
+        // Property syntax provides getters and setters pretending
+        // to be a field
+
+        // we can have properties without set
+        public string Color { get; } = "brown";
+        public string Breed { get; set; }
+
+        // methods
         public void GoTo(string location)
         {
             // easy way to concatenate strings
--- my-training-code/C#/VS/Animals/Animals.UI/Program.cs
+++ my-training-code/C#/VS/Animals/Animals.UI/Program.cs
@@ -8,6 +8,8 @@ namespace Animals.UI
         static void Main(string[] args)
         {
             var dog = new Dog();
+            dog.Name = "Fido";
+            Console.WriteLine($"Dog's name is {dog.Name}");
             dog.GoTo("door");
             dog.MakeNoise();
         }
